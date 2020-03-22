e5e16675ac7461033bc1c7231be929b7a31aa6a4
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-03-22 16:58:07 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -14,11 +14,16 @@ def prompt
   puts "Enter a number from 1 to 6."
 end
   
+  
+  
 def get_guess
     # capture (guess) input from CLI (& remove \n with chomp)
     input = gets.chomp
   input
 end
+  
+  
+  
   
     # compare (guess) input to comp_num
   
